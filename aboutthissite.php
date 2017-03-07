0e6a4ce74091b74885edb89c2111c70e13791719
Unusedaccountname <Unusedaccountname@users.noreply.github.com>
2017-03-06 18:53:46 -0600

    Update aboutthissite.php

--- aboutthissite.php
+++ aboutthissite.php
@@ -17,25 +17,25 @@
 <br/>
 <br/>
 <br/>
-This website aims to serve as a bridge of communication for current students, alumni of <a href="http://www.sfu.ca/siat/prospective_students/degree_options.html"><b> SFU's School of Interactive Arts & Technology</b></a> 
- and prospective undergraduates considering to attend SIAT.
+This website aims to serve as a bridge of communication for current students, alumni of <a href="http://www.tamuc.edu/campusLife/CampusServices/default.aspx"><b> TAMUC</b></a> 
+ and prospective undergraduates considering to attend Texas A & M Commerce.
 
 
 <br/>
 <br/>
-At this websites, prospective students are able to <a href="browsingmethod.php"><b> view profiles </b></a> of current students or alumni. The benefit of this 
+At this websites, prospective students are able to <a href=""><b> view profiles </b></a> of current students or alumni. The benefit of this 
 is that they may search up people who have gone to the same high school as them and understand how these people are
-doing at SIAT.
+doing at TAMUC.
 
 <br/>
 <br/>
-Both prospective students and current/alumni are able to  <a href="registeraccount.php"><b> register with an account </b></a> at this website therefore being able to communicate
+Both prospective students and current/alumni are able to  <a href=""><b> register with an account </b></a> at this website therefore being able to communicate
 more freely with members already with accounts and getting regular updates regarding their goals, ambitions through blog posts.
 
 <br/>
 <br/>
-Besides the provided mySFU blog service, this website integrates Twitter, so that prospective students are also able to access updates from current members'
-more personal life outside SFU.
+Besides the provided TAMUC blog service, this website integrates Twitter, so that prospective students are also able to access updates from current members'
+more personal life outside TAMUC.
 
  <br/>
  <br/>
